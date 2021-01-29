fbf9c614fae406c15962466a9be6465ac2439d31
li.bowei <encounterallen@vip.qq.com>
2021-01-29 14:25:17 +0800

    添加一个公共的建造者

---
+++ java-basic/src/test/java/org/ilmostro/basic/builder/CommonBuilderTest.java
@@ -0,0 +1,23 @@
+package org.ilmostro.basic.builder;
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.ilmostro.basic.User;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author li.bowei
+ **/
+@Slf4j
+public class CommonBuilderTest {
+
+    @Test
+    public void builder(){
+        User build = CommonBuilder.of(User::new)
+                .with(User::setId, 1)
+                .build();
+        log.info("build entity is :{}", build);
+    }
+}
