c7eaddf71f74d78e616ad6217bf59978d07caa7a
li.bowei <encounterallen@vip.qq.com>
2021-03-02 17:34:27 +0800

    配置文件的读取

--- springboot-test-20190925/src/main/java/org/ilmostro/test/service/statemachine/StateMachineConfiguration.java
+++ springboot-test-20190925/src/main/java/org/ilmostro/test/service/statemachine/StateMachineConfiguration.java
@@ -1,24 +1,24 @@
-package org.ilmostro.test.service.statemachine;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.statemachine.config.EnableStateMachine;
-import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
-import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
-
-/**
- * @author li.bowei
- **/
-@Configuration
-@EnableStateMachine
-public class StateMachineConfiguration extends EnumStateMachineConfigurerAdapter<PayStatus, PayEvent> {
-
-    @Override
-    public void configure(StateMachineTransitionConfigurer<PayStatus, PayEvent> transitions) throws Exception {
-        transitions.withExternal()
-                .source(PayStatus.UNPAID).target(PayStatus.CLOSE).event(PayEvent.CLOSE)
-                .and().withExternal()
-                .source(PayStatus.UNPAID).target(PayStatus.PAID).event(PayEvent.PAY)
-                .and().withExternal()
-                .source(PayStatus.PAID).target(PayStatus.REFUND).event(PayEvent.REFUND);
-    }
-}
+//package org.ilmostro.test.service.statemachine;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.statemachine.config.EnableStateMachine;
+//import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
+//import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+//
+///**
+// * @author li.bowei
+// **/
+//@Configuration
+//@EnableStateMachine
+//public class StateMachineConfiguration extends EnumStateMachineConfigurerAdapter<PayStatus, PayEvent> {
+//
+//    @Override
+//    public void configure(StateMachineTransitionConfigurer<PayStatus, PayEvent> transitions) throws Exception {
+//        transitions.withExternal()
+//                .source(PayStatus.UNPAID).target(PayStatus.CLOSE).event(PayEvent.CLOSE)
+//                .and().withExternal()
+//                .source(PayStatus.UNPAID).target(PayStatus.PAID).event(PayEvent.PAY)
+//                .and().withExternal()
+//                .source(PayStatus.PAID).target(PayStatus.REFUND).event(PayEvent.REFUND);
+//    }
+//}
