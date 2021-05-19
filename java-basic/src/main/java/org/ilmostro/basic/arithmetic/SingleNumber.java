9fc98ef76826f686fdca1ba3eee7893a87f5421d
li.bowei <encounterallen@vip.qq.com>
2021-05-19 10:28:28 +0800

    数据结构与算法

--- java-basic/src/main/java/org/ilmostro/basic/arithmetic/SingleNumber.java
+++ java-basic/src/main/java/org/ilmostro/basic/arithmetic/SingleNumber.java
@@ -1,14 +1,75 @@
 package org.ilmostro.basic.arithmetic;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 /**
  * @author li.bowei
  */
 public class SingleNumber {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{1,1,1,2,2,2,6};
+//        int[] nums = new int[]{3,2,2,3};
+//        int len = removeElement(nums, 2);
+//        for (int i = 0; i < len; i++) {
+//            System.out.print(nums[i]);
+//        }
+//        System.out.println(Character.getNumericValue('Z'));
+//        System.out.println(titleToNumber("FXSHRXW"));
+//        int count = 16;
+//        for(int i = 0; i < 10; i++){
+//            count = count << 1;
+//            System.out.println(count);
+//        }
+        int[] arr = new int[]{-1, 0, 3, 5, 9, 12};
+        System.out.println(search(arr, 12));
+    }
+
+    private static int titleToNumber(String title) {
+        char[] charArray = title.toCharArray();
+        int res = 0;
+        for (char c : charArray) {
+            res = res * 26 + (c - 'A' + 1);
+        }
+        return res;
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int targetIndex = nums.length - 1;
+        int index = 0;
+        while (index <= targetIndex) {
+            if (nums[index] == val) {
+                if (nums[targetIndex] == val) {
+                    targetIndex--;
+                } else {
+                    int temp = nums[index];
+                    nums[index] = nums[targetIndex];
+                    nums[targetIndex] = temp;
+                    targetIndex--;
+                    index++;
+                }
+            } else {
+                index++;
+            }
+        }
+        return index;
+    }
+
+
+    public static int search(int[] nums, int target) {
+        return doBinarySearch(nums, target, 0, nums.length - 1);
+    }
+
+    public static int doBinarySearch(int[] arr, int target, int l, int r) {
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return -1;
     }
 }
