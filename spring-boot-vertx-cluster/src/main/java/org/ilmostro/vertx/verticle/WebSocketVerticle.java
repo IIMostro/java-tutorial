c8e7ae9ecd56f8e2449f3dd2fc1a86132cf8e2dd
li.bowei <encounterallen@qq.com>
2021-08-18 20:35:19 +0800

    消息体添加头

--- spring-boot-vertx-cluster/src/main/java/org/ilmostro/vertx/verticle/WebSocketVerticle.java
+++ spring-boot-vertx-cluster/src/main/java/org/ilmostro/vertx/verticle/WebSocketVerticle.java
@@ -3,6 +3,7 @@ package org.ilmostro.vertx.verticle;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.http.ServerWebSocket;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
@@ -51,19 +52,27 @@ public class WebSocketVerticle extends AbstractVerticle {
         if (socket.uri().equals("/ws")) {
             logger.info("ws accept, socket-id:{}", socket.binaryHandlerID());
             instanceSocket.put(socket.binaryHandlerID(), socket);
-        } else{
+        } else {
             socket.reject();
+            socket.end();
+            return;
         }
-        socket.textMessageHandler(message -> vertx.eventBus().publish("ws.socket.message", message))
+        socket.textMessageHandler(message -> vertx.eventBus().publish("ws.socket.message", message,
+                new DeliveryOptions().addHeader("socket-id", socket.binaryHandlerID())))
                 .exceptionHandler(ex -> logger.error("send message error! cause:{}", ex.getMessage()));
         socket.closeHandler(handler -> vertx.eventBus().publish("ws.socket.close", socket.binaryHandlerID()));
     }
 
-    private void message(Vertx vertx){
-        vertx.eventBus().consumer("ws.socket.message", handler -> instanceSocket.values().forEach(var1 -> var1.writeTextMessage(handler.body().toString())));
+    private void message(Vertx vertx) {
+        vertx.eventBus().consumer("ws.socket.message", handler ->
+                instanceSocket.values().stream().filter(var1 -> !var1.binaryHandlerID().equals(handler.headers().get("socket-id")))
+                        .forEach(var1 -> var1.writeTextMessage(handler.body().toString())));
     }
 
-    private void close(Vertx vertx){
-        vertx.eventBus().consumer("ws.socket.close", handler -> instanceSocket.remove(handler.body().toString()));
+    private void close(Vertx vertx) {
+        vertx.eventBus().consumer("ws.socket.close", handler -> {
+            logger.info("ws disconnect, socket-id:{}", handler.body());
+            instanceSocket.remove(handler.body().toString());
+        });
     }
 }
