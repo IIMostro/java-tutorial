8054e0791dc96024b9204cc6a6ddb883717bc505
li.bowei <encounterallen@qq.com>
2021-09-27 17:38:28 +0800

    添加QLExpress

---
+++ java-basic/src/main/java/org/ilmostro/basic/express/AviatorStart.java
@@ -0,0 +1,46 @@
+package org.ilmostro.basic.express;
+
+import com.googlecode.aviator.AviatorEvaluator;
+import com.googlecode.aviator.Options;
+import com.googlecode.aviator.runtime.function.AbstractFunction;
+import com.googlecode.aviator.runtime.type.AviatorDecimal;
+import com.googlecode.aviator.runtime.type.AviatorObject;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author li.bowei
+ */
+@Slf4j
+public class AviatorStart {
+
+    static class Add extends AbstractFunction{
+
+        @Override
+        public String getName() {
+            return "add";
+        }
+
+        @Override
+        public AviatorObject call(Map<String, Object> env, AviatorObject arg1, AviatorObject arg2) {
+            Object left = arg1.getValue(env);
+            Object right = arg2.getValue(env);
+            log.info("left:{}, right:{}, env:{}", left, right, env.keySet());
+            double var1 = Double.parseDouble(left.toString());
+            double var2 = Double.parseDouble(right.toString());
+            return AviatorDecimal.valueOf(var1 +var2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Map<String, Object> param = new HashMap<>();
+        param.put("a", 100);
+        param.put("b", 200.00000002);
+        param.put("c", 2.000003);
+        AviatorEvaluator.setOption(Options.ALWAYS_PARSE_FLOATING_POINT_NUMBER_INTO_DECIMAL, true);
+        Object exec = AviatorEvaluator.execute("a+b*c", param);
+        System.out.println(exec);
+    }
+}
---
+++ java-basic/src/main/java/org/ilmostro/basic/express/QLExpress.java
@@ -0,0 +1,22 @@
+package org.ilmostro.basic.express;
+
+import com.ql.util.express.DefaultContext;
+import com.ql.util.express.ExpressRunner;
+
+/**
+ * @author li.bowei
+ */
+public class QLExpress {
+
+    public static void main(String[] args) throws Exception {
+        ExpressRunner runner = new ExpressRunner(true, true);
+        DefaultContext<String, Object> context = new DefaultContext<>();
+        context.put("a", 100);
+        context.put("b", 200);
+        context.put("c", 3);
+
+        String express = "(a+b)*c";
+        Object execute = runner.execute(express, context, null, true, true);
+        System.out.println(execute);
+    }
+}
