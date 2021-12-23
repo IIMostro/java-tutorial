33f217ba82e60482c85efc27cf913f97712649a6
li.bowei <1306475495@qq.com>
2021-12-23 17:25:45 +0800

    perf(project): dfs强化训练

---
+++ java-basic/src/main/java/org/ilmostro/basic/leetcode/dfs/Solution14.java
@@ -0,0 +1,87 @@
+package org.ilmostro.basic.leetcode.dfs;
+
+import org.ilmostro.basic.algorithm.TreeNode;
+
+import java.util.*;
+
+/**
+ * 给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * <p>
+ * <p>
+ * 输入: [1,2,3,null,5,null,4]
+ * 输出: [1,3,4]
+ * 示例 2:
+ * <p>
+ * 输入: [1,null,3]
+ * 输出: [1,3]
+ * 示例 3:
+ * <p>
+ * 输入: []
+ * 输出: []
+ *  
+ * <p>
+ * 提示:
+ * <p>
+ * 二叉树的节点个数的范围是 [0,100]
+ * -100 <= Node.val <= 100 
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-right-side-view
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author li.bowei
+ */
+public class Solution14 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.right = new TreeNode(5);
+        root.right = new TreeNode(3);
+        root.right.right = new TreeNode(4);
+        List<Integer> integers = new Solution14().rightSideView(root);
+        System.out.println(integers);
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+       return level(root);
+    }
+
+    List<Integer> result = new ArrayList<>();
+
+    public void dfs(TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        if (depth == result.size()) {
+            result.add(root.val);
+        }
+        depth++;
+        dfs(root.right, depth);
+        dfs(root.left, depth);
+    }
+
+
+    public List<Integer> level(TreeNode root) {
+        if (root == null) return Collections.emptyList();
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode next = queue.poll();
+                if(next.left != null)queue.offer(next.left);
+                if(next.right != null) queue.offer(next.right);
+                if(i == 1){
+                    result.add(next.val);
+                }
+            }
+        }
+        return result;
+    }
+}
