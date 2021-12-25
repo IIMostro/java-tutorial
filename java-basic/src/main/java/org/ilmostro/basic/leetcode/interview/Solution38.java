1ecc4c88d8c215ece8f28aa8146ce09a27b48d5e
li.bowei <1306475495@qq.com>
2021-12-25 23:12:08 +0800

    今天leetcode题目

---
+++ java-basic/src/main/java/org/ilmostro/basic/leetcode/interview/Solution38.java
@@ -0,0 +1,57 @@
+package org.ilmostro.basic.leetcode.interview;
+
+import org.ilmostro.basic.algorithm.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * 输入某二叉树的前序遍历和中序遍历的结果，请构建该二叉树并返回其根节点。
+ *
+ * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ *
+ * Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+ * Output: [3,9,20,null,null,15,7]
+ * 示例 2:
+ *
+ * Input: preorder = [-1], inorder = [-1]
+ * Output: [-1]
+ *  
+ *
+ * 限制：
+ *
+ * 0 <= 节点个数 <= 5000
+ *
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zhong-jian-er-cha-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author li.bowei
+ */
+public class Solution38 {
+
+    int[] preorder;
+    Map<Integer, Integer> dic = new HashMap<>();
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.preorder = preorder;
+        for(int i = 0; i < inorder.length; i++)
+            dic.put(inorder[i], i);
+        return recur(0, 0, inorder.length - 1);
+    }
+    TreeNode recur(int root, int left, int right) {
+        if(left > right) return null;                          // 递归终止
+        TreeNode node = new TreeNode(preorder[root]);          // 建立根节点
+        int i = dic.get(preorder[root]);                       // 划分根节点、左子树、右子树
+        node.left = recur(root + 1, left, i - 1);              // 开启左子树递归
+        node.right = recur(root + i - left + 1, i + 1, right); // 开启右子树递归
+        return node;                                           // 回溯返回根节点
+    }
+}
