0532a49afed93149765111ae56d8fca8eca0cfc5
li.bowei <1306475495@qq.com>
2021-12-27 13:50:13 +0800

    perf(project): 简单算法题目

---
+++ java-basic/src/main/java/org/ilmostro/basic/leetcode/primary/Solution3.java
@@ -0,0 +1,53 @@
+package org.ilmostro.basic.leetcode.primary;
+
+/**
+ * 给你一个数组，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [1,2,3,4,5,6,7], k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右轮转 1 步: [7,1,2,3,4,5,6]
+ * 向右轮转 2 步: [6,7,1,2,3,4,5]
+ * 向右轮转 3 步: [5,6,7,1,2,3,4]
+ * 示例 2:
+ * <p>
+ * 输入：nums = [-1,-100,3,99], k = 2
+ * 输出：[3,99,-1,-100]
+ * 解释:
+ * 向右轮转 1 步: [99,-1,-100,3]
+ * 向右轮转 2 步: [3,99,-1,-100]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 105
+ * -231 <= nums[i] <= 231 - 1
+ * 0 <= k <= 105
+ *  
+ * <p>
+ * 进阶：
+ * <p>
+ * 尽可能想出更多的解决方案，至少有 三种 不同的方法可以解决这个问题。
+ * 你可以使用空间复杂度为 O(1) 的 原地 算法解决这个问题吗？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rotate-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author li.bowei
+ */
+public class Solution3 {
+
+    public void rotate(int[] nums, int k) {
+        if (nums == null || nums.length <= 0) return;
+        int[] pre = new int[nums.length];
+        for (int i = 0; i < nums.length; ++i) {
+            pre[(i + k) % nums.length] = nums[i];
+        }
+        System.arraycopy(pre, 0, nums, 0, nums.length);
+    }
+}
