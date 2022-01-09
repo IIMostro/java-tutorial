3428272a9642a8f6cbaa83469907db49e6970649
li.bowei <1306475495@qq.com>
2022-01-09 22:34:20 +0800

    重新看下前缀和的算法

--- java-basic/src/main/java/org/ilmostro/basic/leetcode/interview/SubarraySum.java
+++ java-basic/src/main/java/org/ilmostro/basic/leetcode/interview/SubarraySum.java
@@ -44,4 +44,16 @@ public class SubarraySum {
         }
         return count;
     }
+
+    public int preSum(int[] nums, int k){
+        int count = 0, pre = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int num : nums) {
+            pre += num;
+            if (map.containsKey(pre - k)) count += map.get(pre - k);
+            map.put(pre, map.getOrDefault(pre, 0) + 1);
+        }
+        return count;
+    }
 }
