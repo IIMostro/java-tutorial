0169390c73717bfce62e45b7b83dafb4588b2a24
li.bowei <1306475495@qq.com>
2022-01-13 14:30:40 +0800

    perf(project): 单调栈问题

---
+++ java-basic/src/main/java/org/ilmostro/basic/leetcode/stack/monotonic/NextGreaterElement_I.java
@@ -0,0 +1,99 @@
+package org.ilmostro.basic.leetcode.stack.monotonic;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * nums1 中数字 x 的 下一个更大元素 是指 x 在 nums2 中对应位置 右侧 的 第一个 比 x 大的元素。
+ *
+ * 给你两个 没有重复元素 的数组 nums1 和 nums2 ，下标从 0 开始计数，其中nums1 是 nums2 的子集。
+ *
+ * 对于每个 0 <= i < nums1.length ，找出满足 nums1[i] == nums2[j] 的下标 j ，并且在 nums2 确定 nums2[j] 的 下一个更大元素 。如果不存在下一个更大元素，那么本次查询的答案是 -1 。
+ *
+ * 返回一个长度为 nums1.length 的数组 ans 作为答案，满足 ans[i] 是如上所述的 下一个更大元素 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums1 = [4,1,2], nums2 = [1,3,4,2].
+ * 输出：[-1,3,-1]
+ * 解释：nums1 中每个值的下一个更大元素如下所述：
+ * - 4 ，用加粗斜体标识，nums2 = [1,3,4,2]。不存在下一个更大元素，所以答案是 -1 。
+ * - 1 ，用加粗斜体标识，nums2 = [1,3,4,2]。下一个更大元素是 3 。
+ * - 2 ，用加粗斜体标识，nums2 = [1,3,4,2]。不存在下一个更大元素，所以答案是 -1 。
+ * 示例 2：
+ *
+ * 输入：nums1 = [2,4], nums2 = [1,2,3,4].
+ * 输出：[3,-1]
+ * 解释：nums1 中每个值的下一个更大元素如下所述：
+ * - 2 ，用加粗斜体标识，nums2 = [1,2,3,4]。下一个更大元素是 3 。
+ * - 4 ，用加粗斜体标识，nums2 = [1,2,3,4]。不存在下一个更大元素，所以答案是 -1 。
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums1.length <= nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 104
+ * nums1和nums2中所有整数 互不相同
+ * nums1 中的所有整数同样出现在 nums2 中
+ *  
+ *
+ * 进阶：你可以设计一个时间复杂度为 O(nums1.length + nums2.length) 的解决方案吗？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/next-greater-element-i
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author li.bowei
+ */
+public class NextGreaterElement_I {
+
+    public static void main(String[] args) {
+        int[] nums1 = {4,1,2};
+        int[] nums2 = {1,3,4,2};
+        int[] ints = new NextGreaterElement_I().nextGreaterElement(nums1, nums2);
+    }
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        return stack(nums1, nums2);
+    }
+
+    public int[] forcible(int[] nums1, int[] nums2) {
+        if(nums1 == null || nums1.length <= 0) return nums1;
+        int[] ans = new int[nums1.length];
+        for (int i : nums1) {
+            int j = 0;
+            while(j < nums2.length && nums2[j] != i){
+                j++;
+            }
+            int k = j + 1;
+            while(k < nums2.length && nums2[k] < nums2[j]){
+                k++;
+            }
+            ans[i] = k < nums2.length ? nums2[k] : -1;
+        }
+        return ans;
+    }
+
+    public int[] stack(int[] nums1, int[] nums2){
+        Map<Integer, Integer> map = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            int num = nums2[i];
+            while (!stack.isEmpty() && num >= stack.peek()) {
+                stack.pop();
+            }
+            map.put(num, stack.isEmpty() ? -1 : stack.peek());
+            stack.push(num);
+        }
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; ++i) {
+            res[i] = map.get(nums1[i]);
+        }
+        return res;
+    }
+}
---
+++ java-basic/src/main/java/org/ilmostro/basic/leetcode/top100/DailyTemperatures.java
@@ -0,0 +1,57 @@
+package org.ilmostro.basic.leetcode.top100;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ *
+ * 请根据每日 气温 列表 temperatures ，请计算在每一天需要等几天才会有更高的温度。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+ *
+ * 示例 1:
+ *
+ * 输入: temperatures = [73,74,75,71,69,72,76,73]
+ * 输出: [1,1,4,2,1,1,0,0]
+ * 示例 2:
+ *
+ * 输入: temperatures = [30,40,50,60]
+ * 输出: [1,1,1,0]
+ * 示例 3:
+ *
+ * 输入: temperatures = [30,60,90]
+ * 输出: [1,1,0]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= temperatures.length <= 105
+ * 30 <= temperatures[i] <= 100
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/daily-temperatures
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author li.bowei
+ */
+public class DailyTemperatures {
+
+    public static void main(String[] args) {
+        int[] nums = {73,74,75,71,69,72,76,73};
+        int[] ints = new DailyTemperatures().dailyTemperatures(nums);
+
+    }
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int length = temperatures.length;
+        int[] ans = new int[length];
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < length; i++) {
+            int temperature = temperatures[i];
+            while (!stack.isEmpty() && temperature > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+                ans[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+}
