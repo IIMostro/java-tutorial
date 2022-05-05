94f016527c7adb537200db1ab54be93c2380ed5a
li.bowei <1306475495@qq.com>
2022-05-05 17:23:15 +0800

    多线程下的Handler处理，现在4.0版本的Disruptor没有WorkProcess，所以要基于这种方式来做

--- spring-boot-pure/src/main/java/org/ilmostro/pure/PureApplication.java
+++ spring-boot-pure/src/main/java/org/ilmostro/pure/PureApplication.java
@@ -1,7 +1,5 @@
 package org.ilmostro.pure;
 
-import org.ilmostro.pure.annotation.EnableHttpMeter;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- spring-boot-pure/src/test/java/org/ilmostro/pure/disruptor/DisruptorWorkTest.java
+++ spring-boot-pure/src/test/java/org/ilmostro/pure/disruptor/DisruptorWorkTest.java
@@ -1,13 +1,19 @@
 package org.ilmostro.pure.disruptor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+import com.lmax.disruptor.EventHandler;
 import com.lmax.disruptor.WorkerPool;
 import com.lmax.disruptor.dsl.Disruptor;
 import org.ilmostro.pure.configuration.DisruptorConfiguration;
 import org.ilmostro.pure.domain.GoodsElement;
 import org.junit.Test;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -19,6 +25,7 @@ import java.util.concurrent.TimeUnit;
 public class DisruptorWorkTest {
 
     Disruptor<GoodsElement> disruptor = DisruptorConfiguration.getInstance();
+    private static final int count = 10;
 
 
     @Test
@@ -38,5 +45,42 @@ public class DisruptorWorkTest {
         }
 
         TimeUnit.SECONDS.sleep(3);
+    }
+
+    @Test
+    public void parallel() throws Exception{
+        List<ParallelEventHandler> handlers = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            handlers.add(new ParallelEventHandler(count, i));
+        }
+        disruptor.handleEventsWith(handlers.toArray(new ParallelEventHandler[0]));
+        disruptor.start();
+        for (int i = 0; i < 100; i++) {
+            final int fi = i;
+            disruptor.publishEvent(((event, sequence) -> event.setId(fi)));
+        }
+        TimeUnit.SECONDS.sleep(3);
+    }
+
+    static class ParallelEventHandler implements EventHandler<GoodsElement>{
+
+        private static final Logger logger = LoggerFactory.getLogger(ParallelEventHandler.class);
+
+        private final int mask;
+        private final int ordinal;
+
+        ParallelEventHandler(int mask, int ordinal) {
+            this.mask = mask;
+            this.ordinal = ordinal;
+        }
+
+        @Override
+        public void onEvent(GoodsElement event, long sequence, boolean endOfBatch) throws Exception {
+            if ((sequence & mask) != ordinal){
+                return;
+            }
+            LockSupport.parkNanos(1);
+            logger.info("current thread:{}, event:{}", Thread.currentThread().getName(), event);
+        }
     }
 }
