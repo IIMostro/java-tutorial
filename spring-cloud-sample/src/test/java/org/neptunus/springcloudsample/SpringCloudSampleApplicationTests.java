8eb7bf7f57f3a539a2b76d219abb28eebba61d40
li.bowei <1306475495@qq.com>
2022-06-21 09:57:27 +0800

    spring cloud sample 可以启动了

---
+++ spring-cloud-sample/src/test/java/org/neptunus/springcloudsample/SpringCloudSampleApplicationTests.java
@@ -0,0 +1,14 @@
+package org.neptunus.springcloudsample;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
