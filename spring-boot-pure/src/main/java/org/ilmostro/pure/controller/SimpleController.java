d921b9542a380f09544757327526d2383e1a9f0e
li.bowei <1306475495@qq.com>
2023-02-02 16:06:17 +0800

    prometheus监控的一些使用

--- spring-boot-pure/src/main/java/org/ilmostro/pure/controller/SimpleController.java
+++ spring-boot-pure/src/main/java/org/ilmostro/pure/controller/SimpleController.java
@@ -1,8 +1,15 @@
 package org.ilmostro.pure.controller;
 
+import java.util.List;
+
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Measurement;
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.vertx.core.Vertx;
 import lombok.extern.slf4j.Slf4j;
 import org.ilmostro.pure.service.SimpleService;
@@ -22,10 +29,13 @@ public class SimpleController {
     private final Vertx vertx;
     private final SimpleService service;
 
+    private final MeterRegistry registry;
+
     public SimpleController(@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") Vertx vertx,
-            SimpleService service) {
+            SimpleService service, MeterRegistry registry) {
         this.vertx = vertx;
         this.service = service;
+        this.registry = registry;
     }
 
     @GetMapping()
@@ -42,18 +52,29 @@ public class SimpleController {
     }
 
     @GetMapping("/now")
-//    @Counted(value = "custom_now_counted")
+    @Counted(value = "normal", extraTags = {"path", "normal"})
     public String now(){
         final Marker mark = MarkerFactory.getMarker("now-");
         log.info(mark, "current time: {}", System.nanoTime());
         return service.get("hello!");
     }
 
+    @GetMapping("/metrics")
+    public String metrics(){
+        final List<Meter> meters = registry.getMeters();
+        for (Meter meter : meters) {
+            for (Measurement measurement : meter.measure()) {
+                log.info("measurement name:[{}] :[{}], value:[{}]",meter.getId().getName(), measurement.getStatistic().getTagValueRepresentation(), measurement.getValue());
+            }
+        }
+        return null;
+    }
+
     @GetMapping("/throw")
-//    @Counted(value = "custom_throw_counted", recordFailuresOnly = true)
-    public String exception(){
+    @Counted(value = "throw.error.controller", recordFailuresOnly = true, extraTags = {"path", "throw"})
+    public String exception(Integer number){
         log.info("current time: {}", System.nanoTime());
-        exc();
+        if (number % 2 == 0) exc();
         return String.valueOf(System.nanoTime());
     }
 
