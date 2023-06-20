c4c4173e5d08c28af2607d0bfd40ecc3c3de439f
lbw7535 <1306475495@qq.com>
2023-06-20 14:39:36 +0800

    perf: 添加traceId

--- spring-boot-pure/src/main/java/org/ilmostro/pure/configuration/WebClientLoggingInterceptor.java
+++ spring-boot-pure/src/main/java/org/ilmostro/pure/configuration/WebClientLoggingInterceptor.java
@@ -7,15 +7,37 @@ import io.vertx.ext.web.client.impl.MultipartFormUpload;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.reflect.FieldUtils;
+import org.slf4j.MDC;
+import org.springframework.http.MediaType;
 
 import java.util.Objects;
+import java.util.UUID;
 
 @Slf4j
 public class WebClientLoggingInterceptor implements Handler<HttpContext<?>> {
+
+    private final String trace;
+    private static final String TRACE_ID = "TraceId";
+
+    public WebClientLoggingInterceptor(String trace) {
+        this.trace = trace;
+    }
+
+    public WebClientLoggingInterceptor(){
+        this(TRACE_ID);
+    }
+
+
     @Override
     public void handle(HttpContext<?> event) {
         final var phase = event.phase();
+        // 此时的线程还在main中
+        if (phase.equals(ClientPhase.CREATE_REQUEST)){
+            final var traceId = MDC.get(trace);
+            event.set(trace, StringUtils.isBlank(traceId) ? UUID.randomUUID().toString() : traceId);
+        }
         if (phase.equals(ClientPhase.SEND_REQUEST)) {
+            event.set("TIME", System.currentTimeMillis());
             printRequest(event);
         }
         if (phase.equals(ClientPhase.DISPATCH_RESPONSE)) {
@@ -24,16 +46,16 @@ public class WebClientLoggingInterceptor implements Handler<HttpContext<?>> {
         event.next();
     }
 
-    private static void printRequest(HttpContext<?> event){
+    private void printRequest(HttpContext<?> event){
         try{
-            event.set("TIME", System.currentTimeMillis());
+            MDC.put(trace, event.get(trace));
             log.info(" --> {} {}", event.requestOptions().getMethod(), event.requestOptions().getHost());
             final var body = event.body();
             if (Objects.nonNull(body)) {
                 final var contentType = event.requestOptions().getHeaders().get("Content-Type");
                 log.info(" Content-Type: {}", contentType);
                 log.info(" Content-Length: {}", event.requestOptions().getHeaders().get("Content-Length"));
-                if (StringUtils.equals(contentType, "application/x-www-form-urlencoded") &&
+                if (StringUtils.equals(contentType, MediaType.APPLICATION_FORM_URLENCODED_VALUE) &&
                         body instanceof MultipartFormUpload) {
                     final var field = FieldUtils.getDeclaredField(MultipartFormUpload.class, "encoder", true);
                     try {
@@ -53,8 +75,9 @@ public class WebClientLoggingInterceptor implements Handler<HttpContext<?>> {
         }
     }
 
-    private static void printResponse(HttpContext<?> event) {
+    private  void printResponse(HttpContext<?> event) {
         try {
+            MDC.put(trace, event.get(trace));
             log.info(" <-- {} {} {} {}ms", event.response().statusCode(), event.response().statusMessage(), event.requestOptions().getHost(), System.currentTimeMillis() - event.<Long>get("TIME"));
             log.info(" Content-Type: {}", event.response().getHeader("Content-Type"));
             log.info(" Set Cookies: {}", event.response().getHeader("Set-Cookie"));
