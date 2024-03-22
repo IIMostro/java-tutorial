29573e0c87331f0b39a656d5ff434d4cff94de13
li.bowei <1306475495@qq.com>
2024-03-22 14:09:26 +0800

    perf: spring cache redis metrics
    1. spring boot cache配合redis的时候添加监控

--- spring-boot-redis/src/main/java/org/ilmostro/redis/configuration/MetricsConfiguration.java
+++ spring-boot-redis/src/main/java/org/ilmostro/redis/configuration/MetricsConfiguration.java
@@ -2,17 +2,28 @@ package org.ilmostro.redis.configuration;
 
 import io.micrometer.core.instrument.Tag;
 import org.springframework.boot.actuate.metrics.cache.RedisCacheMetrics;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCache;
 import org.springframework.data.redis.cache.RedisCacheManager;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Configuration
 public class MetricsConfiguration {
     @Bean
-    public RedisCacheMetrics redisCacheMetrics(RedisCacheManager cacheManager) {
-//        return new RedisCacheMetrics((RedisCache) cacheManager.getCache("magik"), List.of(Tag.of("application", "magik")));
+    public List<RedisCacheMetrics> redisCacheMetrics(RedisCacheManager cacheManager, CacheProperties properties, ApplicationContext context) {
+        return Optional.of(properties)
+                .map(CacheProperties::getCacheNames)
+                .orElseGet(Collections::emptyList)
+                .stream()
+                .map(cacheManager::getCache)
+                .filter(cache -> cache instanceof RedisCache)
+                .map(cache -> new RedisCacheMetrics((RedisCache) cache, List.of(Tag.of("application", context.getApplicationName()))))
+                .toList();
     }
 }
